d7c49227ea2b56bf72635f9949540f22e39da76d
thuthuin9601 <96036874+thuthuin9601@users.noreply.github.com>
2022-02-23 10:27:03 +0700

    update1

---
+++ script.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function(){
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    const CANVAS_WIDTH = canvas.width = 768;
+    const CANVAS_HEIGHT = canvas.height = 576;
+})
